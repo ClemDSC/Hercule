9cadd414ec993a87fbf68be5b882b2775f142369
ClemDSC <clemence.p14@gmail.com>
2022-04-07 11:45:40 +0200

    heracles part 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const Fighter = require("./src/Fighter.js");
+const Weapon = require("./src/Weapon.js");
+const Shield = require("./src/Shield.js");
+
+const weapon = new Weapon("sword");
+const shield = new Shield("blabla");
 
 /** Create Heracles  */
-const heracles = new Fighter("🧔 Heracles", 20, 6);
+const heracles = new Fighter("🧔 Heracles", 20, 6, weapon.damage, shield.protection);
 
 /** Create the opponent  */
 const boar = new Fighter("🐗 Erymanthian Boar", 25, 12);
@@ -25,3 +30,24 @@ const score = (fighter1, fighter2) => {
     loser: fighter1
   };
 };
+
+{
+  let round = 0;
+
+  while (heracles.isAlive() && boar.isAlive()) {
+      console.log(`🕛 Round #${round}`);
+
+      heracles.fight(boar);
+      console.log(roundDisplay(heracles, boar));
+
+      boar.fight(heracles);
+      console.log(roundDisplay(boar, heracles));
+
+      round++;
+  }
+
+  const result = score(heracles, boar);
+
+  console.log(`💀 ${result.loser.name} is dead`);
+  console.log(`🏆 ${result.winner.name} wins (💙 ${result.winner.life} )`);
+}
